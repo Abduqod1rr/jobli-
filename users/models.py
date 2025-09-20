ea061f00237ede3382f133c13184ab494b390e5d
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-20 20:42:05 +0500

    JUST A commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES=(
+        ("job seeker","Job seeker"),
+        ("job owner","Job owner" ),
+    )
+    role=models.CharField(max_length=30, choices=ROLE_CHOICES, default="job seeker")
+    
+    def __str__(self):
+        return f"{self.username} : {self.role}"
