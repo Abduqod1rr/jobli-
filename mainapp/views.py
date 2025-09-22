afd8f220f5c76ed4f2c9ebeee36d2bcbd5020b2d
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-22 08:33:21 +0500

    w

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render ,HttpResponse
-
+from django.views.generic import ListView ,CreateView ,UpdateView , DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Job , Apply
 # Create your views here.
 def home(request):
     return HttpResponse("hello world")
+
+class HomeView(LoginRequiredMixin,ListView):
+    model=Job
+    template_name='home.html'
+    context_object_name='job'
  
