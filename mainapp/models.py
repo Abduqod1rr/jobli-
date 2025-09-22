cb4601e4a35647c7a97edcafe4583153e6218b89
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-22 08:16:57 +0500

    c

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+class Job(models.Model):
+    title=models.CharField(max_length=30)
+    about=models.TextField()
+    posted_at=models.DateTimeField(auto_now_add=True)
+    owner=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title,self.owner
