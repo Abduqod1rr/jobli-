0594742798c7995675093ad53f99d1ebf622ebb7
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-22 08:22:17 +0500

    c

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,4 +8,13 @@ class Job(models.Model):
     owner=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title,self.owner
+        return f"{self.title} | {self.owner}"
+    
+class Apply(models.Model):
+    job=models.ForeignKey(Job,on_delete=models.CASCADE)
+    user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    letter=models.TextField()
+    applied_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user
